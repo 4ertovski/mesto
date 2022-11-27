0c0dab3ab8a431310d56c9d07e63c8af643d4714
Lesia <lisa4ertovski@gmail.com>
2022-11-27 15:55:21 +0300

    исправить верстку

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const popup = document.querySelector ('.popup');
 const buttonEditOpen = document.querySelector ('.profile__button_active_edit');
 const buttonClose = document.querySelector ('.popup__button_active_exit');
 const getName = document.querySelector ('.profile__name');
-const getJob = document.querySelector ('.profile__definition');
+const getTitle = document.querySelector ('.profile__definition');
 const editForm = document.querySelector ('.popup__container');
 let nameInput = editForm.querySelector ('.popup__input_name');
 let jobInput =  editForm.querySelector ('.popup__input_job');
@@ -10,7 +10,7 @@ let jobInput =  editForm.querySelector ('.popup__input_job');
 function openPopup () {
   popup.classList.add ('popup__opened');
   nameInput.value = getName.textContent;
-  jobInput.value = getJob.textContent;
+  jobInput.value = getTitle.textContent;
 }
 
 function closePopup ()  {
@@ -20,7 +20,7 @@ popup.classList.remove ('popup__opened');
 function formSubmitHandler (evt) {
     evt.preventDefault();
     getName.textContent = `${nameInput.value}`;
-    getJob.textContent = `${jobInput.value}`;
+    getTitle.textContent = `${jobInput.value}`;
     closePopup ();
 }
 
