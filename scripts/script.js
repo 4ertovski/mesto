3e0b087f70560078aa5d8f8f512b44a82233183a
Lesia <lisa4ertovski@gmail.com>
2022-11-30 11:21:42 +0300

    Исправление после второго ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
 const popup = document.querySelector ('.popup');
 const buttonEditOpen = document.querySelector ('.profile__button_active_edit');
 const buttonClose = document.querySelector ('.popup__button_active_exit');
-const getName = document.querySelector ('.profile__name');
-const getTitle = document.querySelector ('.profile__definition');
+const profileName = document.querySelector ('.profile__name');
+const profileTitle = document.querySelector ('.profile__definition');
 const editForm = document.querySelector ('.popup__form');
-let nameInput = editForm.querySelector ('.popup__input-name');
-let titleInput =  editForm.querySelector ('.popup__input-title');
+const nameInput = editForm.querySelector ('.popup__input-name');
+const titleInput =  editForm.querySelector ('.popup__input-title');
 
 function openPopup () {
   popup.classList.add ('popup_opened');
-  nameInput.value = getName.textContent;
-  titleInput.value = getTitle.textContent;
+  nameInput.value = profileName.textContent;
+  titleInput.value = profileTitle.textContent;
 }
 
 function closePopup ()  {
@@ -19,8 +19,8 @@ popup.classList.remove ('popup_opened');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    getName.textContent = `${nameInput.value}`;
-    getTitle.textContent = `${titleInput.value}`;
+    profileName.textContent = `${nameInput.value}`;
+    profileTitle.textContent = `${titleInput.value}`;
     closePopup ();
 }
 
